e83c549a07ba33b5b9d157a5e7f98611002bd5f0
Jhonnyel Rodriguez <jhonnyel.r@gmail.com>
2018-07-04 15:06:22 -0400

    Primer Guardado

---
+++ practica 1/Script1.js
@@ -0,0 +1,19 @@
+
+//Datos
+var a = 0;
+var b = 0;
+var resultado = 0;
+a = prompt('Ingrese Primer Numero');
+b = prompt('Ingrese Segundo Numero');
+
+//Precedimiento
+if (a < b) {
+	resultado = a + b;
+	alert('El Resultado es: '+resultado);
+}
+else{
+	resultado = a - b;
+	alert('El Resultado es: '+resultado);
+}
+
+
